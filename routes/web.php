a5c65ab0d529c518600eb92deb0d1ba908d6feca
kyawphyowai94.github.io <kyawphyowai.sgg@gmail.com>
2024-12-04 16:36:43 +0630

    ITBlog tables

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[App\Http\Controllers\FrontController::class,'itBlog'])->name('itBlog');
 Route::get('/Blog-post/{id}',[App\Http\Controllers\FrontController::class,'blogItem'])->name('blogItem');
 
+Route::group(['prefix'=>'backend','as'=>'backend.'],function(){
+    
+    Route::get('/',[App\Http\Controllers\Admin\DashboardController::class,'index'])->name('dashboard');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
